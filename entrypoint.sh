8cf1455b45fc102304dc6498777460e46ab0041d
Jason Wright <jwright@speareducation.com>
2020-07-08 13:11:22 -0700

    GL-1451 added action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 [ ! -f vendor/bin/phpunit ] && echo 'vendor/bin/phpunit not found' && exit 1
 
-mkdir .test-results
+mkdir $INPUT_TARGETDIR
 
 if [ -d storage ]
 then
     mkdir -p storage/logs
-    ln -s .test-results/logs storage/logs
+    ln -s $INPUT_TARGETDIR/logs storage/logs
 fi
 
 cp .env .env.bak 2>/dev/null
@@ -16,6 +16,6 @@ cp .env.github .env
 php artisan --env=testing migrate
 
 vendor/bin/phpunit \
-    --coverage-html=.test-results/coverage-html \
-    --coverage-text=.test-results/coverage.txt |\
-    tee .test-results/phpunit-results.txt
+    --coverage-html=$INPUT_TARGETDIR/coverage-html \
+    --coverage-text=$INPUT_TARGETDIR/coverage.txt |\
+    tee $INPUT_TARGETDIR/phpunit-results.txt
