7f6a9c70004064e321e80445a5f21e8b11517fd7
Jason Wright <jwright@speareducation.com>
2020-07-09 12:55:52 -0700

    php-fpm_1.11

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 [ ! -f vendor/bin/phpunit ] && echo 'vendor/bin/phpunit not found' && exit 1
 
-mkdir $INPUT_TARGETDIR
-
-if [ -d storage ]
-then
-    mkdir -p storage/logs
-    ln -s $INPUT_TARGETDIR/logs storage/logs
-fi
-
 cp .env .env.bak 2>/dev/null
 cp .env.github .env.testing
 cp .env.github .env
 
 php artisan --env=testing migrate
 
-vendor/bin/phpunit \
-    --coverage-html=$INPUT_TARGETDIR/coverage-html \
-    --coverage-text=$INPUT_TARGETDIR/coverage.txt |\
-    tee $INPUT_TARGETDIR/phpunit-results.txt
+if [ -z "$INPUT_TARGETDIR" ]
+then
+    outputFile=""
+else
+    mkdir $INPUT_TARGETDIR
+
+    if [ -d storage ]
+    then
+        mkdir -p storage/logs
+        ln -s $INPUT_TARGETDIR/logs storage/logs
+    fi
+
+    outputFile="$INPUT_TARGETDIR/phpunit-results.txt"
+fi
+
+vendor/bin/phpunit $INPUT_OPTIONS | tee $outputFile
