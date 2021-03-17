d443b80f9c81abb650b6b76901c7d5cf11cf8446
Jason Wright <jwright@speareducation.com>
2021-03-17 15:47:37 -0700

    GL-1839 updated to output code coverage

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,12 @@ else
     outputFile="$INPUT_TARGETDIR/phpunit-results.txt"
 fi
 
-vendor/bin/phpunit $INPUT_OPTIONS | tee "$outputFile"
+vendor/bin/phpunit \
+    --coverage-text="$INPUT_TARGETDIR/coverage.txt" \
+    --coverage-html="$INPUT_TARGETDIR/coverage-html" |\
+    tee "$outputFile"
 
+COVERAGE=$(head -n9 "$INPUT_TARGETDIR/coverage.txt" | egrep '^  Lines' | awk '{ print $2 }' | sed 's/%//g')
 SUCCESS=$([ "$(fgrep OK "$outputFile")" != "" ] && echo 'true' || echo 'false')
 SUMMARY=$(grep -i 'assertions' "$outputFile" | grep -i 'tests')
 RESULT_TEXT=$([ "$SUCCESS" == "true" ] && echo "PASS" || echo "FAIL")
@@ -35,4 +39,4 @@ echo ::set-output name=success::${SUCCESS}
 echo ::set-output name=summary::${SUMMARY}
 echo ::set-output name=result-text::${RESULT_TEXT}
 echo ::set-output name=result-emoji::${RESULT_EMOJI}
-
+echo ::set-output name=coverage::${COVERAGE}
